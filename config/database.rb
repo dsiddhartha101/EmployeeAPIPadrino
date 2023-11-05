d37e4be1ed3e3179b5a9ca7a873904478cf5e9c2
dsiddhartha101 <siddhartha.das@highspot.com>
2023-11-05 23:49:59 +0530

    database configs made cleaner

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,12 @@
 # MongoMapper.connection = Mongo::Client.new('localhost', 27017)
-MongoMapper.connection = Mongo::Client.new(['localhost:27017'], database: 'personal_learning_dev')
+
+db_name = 'personal_learning_dev'
+db_host = 'localhost'
+db_port = 27017
+MongoMapper.connection = Mongo::Client.new(["#{db_host}:#{db_port}"], database: db_name)
+
 case Padrino.env
-  when :development then MongoMapper.database = 'personal_learning_dev'
-  when :production  then MongoMapper.database = 'personal_learning_dev'
-  when :test        then MongoMapper.database = 'personal_learning_dev'
+  when :development then MongoMapper.database = db_name
+  when :production  then MongoMapper.database = db_name
+  when :test        then MongoMapper.database = db_name
 end
